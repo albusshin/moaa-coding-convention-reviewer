6ecae23a849d4b04a41519873f8e1cf253d9a90d
Shin <albusshin@gmail.com>
2014-01-15 15:05:06 +0100

    Added paths to use at workplace

--- src/reviewer/core.clj
+++ src/reviewer/core.clj
@@ -16,12 +16,15 @@
     (with-open [rdr (clojure.java.io/reader filename)]
       (doseq [line (line-seq rdr)]
         (if (clojure.contrib.string/substring? "todo" (first (codeComment (clojure.string/lower-case line))))
-          (def lines (conj lines (str line "! unfinished TODO" )))
+          (def lines (conj lines (str line " !unfinished TODO" )))
           (def lines (conj lines line))))))
-    (with-open [wrtr (clojure.java.io/writer "/home/albus/Desktop/dummy")]
+    (with-open [wrtr #_(clojure.java.io/writer "/home/albus/Desktop/dummy")
+                #_(clojure.java.io/writer "C:\\Users\\xinti\\Desktop\\dummy")
+               (clojure.java.io/writer filename) ]
       (doseq [line lines]
         (.write wrtr (str line "\n")))))
 
 (defn -main
   [& args]
-  (unfinishedTodos "/home/albus/Desktop/ChartererController.cs"))
+  #_(unfinishedTodos "/home/albus/Desktop/ChartererController.cs")
+  (unfinishedTodos "C:\\Users\\xinti\\Desktop\\ChartererController.cs"))
