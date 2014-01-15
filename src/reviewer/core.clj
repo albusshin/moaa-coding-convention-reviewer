025664a792bda36e2f70383ec706e9e817d975ba
Albus Shin <AlbusShin@gmail.com>
2014-01-15 01:04:14 +0100

    codeComment and writer

--- src/reviewer/core.clj
+++ src/reviewer/core.clj
@@ -2,12 +2,26 @@
   (:require clojure.contrib.string)
   (:gen-class))
 
-(defn unfinishedtodos [filename]
-  (with-open [rdr (clojure.java.io/reader filename)]
-    (doseq [line (line-seq rdr)]
-      (if (clojure.contrib.string/substring? "todo" (clojure.string/lower-case line))
-       (println "unfinished todo") (println "nothing")))))
+(defn codeComment
+  "return the comment part of code passed in"
+  [code]
+  (if(clojure.contrib.string/substring? "//" code)
+    (rest(clojure.contrib.string/split #"//" code))
+    [""]))
+
+(defn unfinishedTodos
+  "Find all unfinished TODOs in current file and then add a comment emphasizing the task"
+  [filename]
+  (do (def lines [])
+    (with-open [rdr (clojure.java.io/reader filename)]
+      (doseq [line (line-seq rdr)]
+        (if (clojure.contrib.string/substring? "todo" (first (codeComment (clojure.string/lower-case line))))
+          (conj lines (concat line "! unfinished TODO" ))
+          (conj lines line))))
+    (with-open [wrtr (clojure.java.io/writer "/home/albus/Desktop/dummy")]
+      (doseq [line lines]
+        (.write wrtr line)))))
 
 (defn -main
   [& args]
-  (unfinishedtodos "/home/albus/Desktop/ChartererController.cs"))
+  (unfinishedTodos "/home/albus/Desktop/ChartererController.cs"))
