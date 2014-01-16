feac9ec2c1b46b41ceacac9e636ea80f1afb2918
Albus Shin <AlbusShin@gmail.com>
2014-01-16 23:59:28 +0100

    changed project file, removed dependency from clojure.contrib

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :url "https://github.com/albusshin/Reviewerx"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :main ^:skip-aot reviewer.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
