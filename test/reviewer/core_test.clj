38c16fdf233ac93089fb88752c12659e61145305
Shin <albusshin@gmail.com>
2014-01-17 17:29:07 +0100

    Composed related tests together

--- test/reviewer/core_test.clj
+++ test/reviewer/core_test.clj
@@ -2,18 +2,15 @@
   (:require [clojure.test :refer :all]
             [reviewer.core :refer :all]))
 
-(deftest test-code-comment-with-cs-file
-  (testing "test if code-comments returns correct value with .cs files"
+(deftest test-code-comment-with-file-extensions
+  (testing "test if code-comments returns correct value with file and file extensions"
     (is (= ["//foo"] 
            (code-comments "C#Code//foo\nGetAll()" "cs")))
     (is (= ["//bar"] 
            (code-comments "//bar" "cs")))
     (is (= #{"/*foo*/" "//bar"} 
            (into #{} (code-comments "//bar\nabcdefg/*foo*/fdsa" "cs"))))
-    (is (nil? (code-comments "abcdefg" "cs")))))
-
-
-(deftest test-code-comment-with-cshtml-file
+    (is (nil? (code-comments "abcdefg" "cs"))))
   (testing "test if code-comments returns correct value with .cshtml files"
     (is (= ["<!--foo-->"]
            (code-comments "C#View Code<!--foo-->Display(x => x.Name)" "cshtml")))
@@ -22,31 +19,23 @@
     (is (= #{"@*foo*@" "<!--bar-->"}
            (into #{} (code-comments "<Whateveritis<!--bar-->AndDisplayx=>x.Name@*foo*@fdsa" "cshtml"))))
     (is nil?
-        (code-comments "abcdefg" "cshtml"))))
-
-(deftest test-code-comment-with-html-file
+        (code-comments "abcdefg" "cshtml")))
   (testing "test if code-comments returns correct value with .html files"
     (is (= ["<!--foo-->"]
            (code-comments "C#View Code<!--foo-->Display(x => x.Name)" "html")))
     (is (= #{"<!--foo-->" "<!--bar-->"}
            (into #{} (code-comments "<What<!--foo-->everitis<!--bar-->AndDisplayx=>x.Name@*foo*@fdsa" "html"))))
-    (is nil? (code-comments "abcdefg" "html"))))
-
-(deftest test-code-comment-with-xml-file
+    (is nil? (code-comments "abcdefg" "html")))
   (testing "test if code-comments returns correct value with .xml files"
     (is (= ["<!--foo-->"]
            (code-comments "C#View Code<!--foo-->Display(x => x.Name)" "xml")))
     (is (= #{"<!--foo-->" "<!--bar-->"}
            (into #{} (code-comments "<What<!--foo-->everitis<!--bar-->AndDisplayx=>x.Name@*foo*@fdsa" "xml"))))
-    (is nil? (code-comments "abcdefg" "xml"))))
-
-(deftest test-code-comment-with-css-file
+    (is nil? (code-comments "abcdefg" "xml")))
   (testing "test if code-comments returns correct value with .css files"
     (is (= #{"/*foo*/" "/*bar*/"}
            (into #{} (code-comments "/*foo*/What the foo is this? /*bar*/ Its a bar" "css"))))
-    (is nil? (code-comments "abcdefg" "css"))))
-
-(deftest test-code-comment-with-js-file
+    (is nil? (code-comments "abcdefg" "css")))
   (testing "test if code-comments returns correct value with .js files"
     (is (= ["//foo"]
            (code-comments "C#Code//foo\nGetAll()" "js")))
