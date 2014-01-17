f36c655b42746ca08d477d053958a4d1d119dd43
Shin <albusshin@gmail.com>
2014-01-17 17:20:16 +0100

    formatted code

--- test/reviewer/core_test.clj
+++ test/reviewer/core_test.clj
@@ -4,62 +4,57 @@
 
 (deftest test-code-comment-with-cs-file
   (testing "test if code-comments returns correct value with .cs files"
-    (is (= ["//foo"]
-         (code-comments "C#Code//foo\nGetAll()" "cs")))
-    (is (= ["//bar"]
-         (code-comments "//bar" "cs")))
-    (is (= #{"/*foo*/" "//bar"}
-         (into #{} (code-comments "//bar\nabcdefg/*foo*/fdsa" "cs"))))
-    (is (nil?
-         (code-comments "abcdefg" "cs")))))
+    (is (= ["//foo"] 
+           (code-comments "C#Code//foo\nGetAll()" "cs")))
+    (is (= ["//bar"] 
+           (code-comments "//bar" "cs")))
+    (is (= #{"/*foo*/" "//bar"} 
+           (into #{} (code-comments "//bar\nabcdefg/*foo*/fdsa" "cs"))))
+    (is (nil? (code-comments "abcdefg" "cs")))))
 
 
 (deftest test-code-comment-with-cshtml-file
   (testing "test if code-comments returns correct value with .cshtml files"
     (is (= ["<!--foo-->"]
-         (code-comments "C#View Code<!--foo-->Display(x => x.Name)" "cshtml")))
+           (code-comments "C#View Code<!--foo-->Display(x => x.Name)" "cshtml")))
     (is (= ["@*bar*@"]
-         (code-comments "//b<!baaaaaar@*bar*@ar" "cshtml")))
+           (code-comments "//b<!baaaaaar@*bar*@ar" "cshtml")))
     (is (= #{"@*foo*@" "<!--bar-->"}
-         (into #{} (code-comments "<Whateveritis<!--bar-->AndDisplayx=>x.Name@*foo*@fdsa" "cshtml"))))
+           (into #{} (code-comments "<Whateveritis<!--bar-->AndDisplayx=>x.Name@*foo*@fdsa" "cshtml"))))
     (is nil?
         (code-comments "abcdefg" "cshtml"))))
 
 (deftest test-code-comment-with-html-file
   (testing "test if code-comments returns correct value with .html files"
     (is (= ["<!--foo-->"]
-         (code-comments "C#View Code<!--foo-->Display(x => x.Name)" "html")))
+           (code-comments "C#View Code<!--foo-->Display(x => x.Name)" "html")))
     (is (= #{"<!--foo-->" "<!--bar-->"}
-         (into #{} (code-comments "<What<!--foo-->everitis<!--bar-->AndDisplayx=>x.Name@*foo*@fdsa" "html"))))
-    (is nil?
-        (code-comments "abcdefg" "html"))))
+           (into #{} (code-comments "<What<!--foo-->everitis<!--bar-->AndDisplayx=>x.Name@*foo*@fdsa" "html"))))
+    (is nil? (code-comments "abcdefg" "html"))))
 
 (deftest test-code-comment-with-xml-file
   (testing "test if code-comments returns correct value with .xml files"
     (is (= ["<!--foo-->"]
-         (code-comments "C#View Code<!--foo-->Display(x => x.Name)" "xml")))
+           (code-comments "C#View Code<!--foo-->Display(x => x.Name)" "xml")))
     (is (= #{"<!--foo-->" "<!--bar-->"}
-         (into #{} (code-comments "<What<!--foo-->everitis<!--bar-->AndDisplayx=>x.Name@*foo*@fdsa" "xml"))))
-    (is nil?
-        (code-comments "abcdefg" "xml"))))
+           (into #{} (code-comments "<What<!--foo-->everitis<!--bar-->AndDisplayx=>x.Name@*foo*@fdsa" "xml"))))
+    (is nil? (code-comments "abcdefg" "xml"))))
 
 (deftest test-code-comment-with-css-file
   (testing "test if code-comments returns correct value with .css files"
     (is (= #{"/*foo*/" "/*bar*/"}
-         (into #{} (code-comments "/*foo*/What the foo is this? /*bar*/ Its a bar" "css"))))
-    (is nil?
-        (code-comments "abcdefg" "css"))))
+           (into #{} (code-comments "/*foo*/What the foo is this? /*bar*/ Its a bar" "css"))))
+    (is nil? (code-comments "abcdefg" "css"))))
 
 (deftest test-code-comment-with-js-file
   (testing "test if code-comments returns correct value with .js files"
     (is (= ["//foo"]
-         (code-comments "C#Code//foo\nGetAll()" "js")))
+           (code-comments "C#Code//foo\nGetAll()" "js")))
     (is (= ["//bar"]
-         (code-comments "//bar" "js")))
+           (code-comments "//bar" "js")))
     (is (= #{"/*foo*/" "//bar"}
-         (into #{} (code-comments "//bar\nabcdefg/*foo*/fdsa" "js"))))
-    (is nil?
-        (code-comments "abcdefg" "js"))))
+           (into #{} (code-comments "//bar\nabcdefg/*foo*/fdsa" "js"))))
+    (is nil? (code-comments "abcdefg" "js"))))
 
 (deftest test-unfinished-todos-in?
   (testing "test if unfinished-todo-in? work properly"
